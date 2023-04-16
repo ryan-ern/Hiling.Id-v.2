672499a2c4c947a993bc1ce9cc3c74b7f164416a
ryan-ern <ryanernanda100902@gmail.com>
2023-04-16 18:49:17 +0700

    update-fix

--- Hiling.Id-V.ReduxSaga/src/redux-saga/ipData/actions.test.js
+++ Hiling.Id-V.ReduxSaga/src/redux-saga/ipData/actions.test.js
@@ -48,10 +48,6 @@ describe("actions", () => {
   it("Melakukan pengiriman aksi fetch IP ketika pengambilan IP berhasi", async () => {
     const ipData = { ip: "127.0.0.1" };
     const mock = mockAxios.onGet("http://ip-api.com/json/").reply(200, ipData);
-    const expectedActions = [
-      { type: "FETCH_IP_DATA_REQUEST" },
-      { type: "FETCH_IP_DATA_SUCCESS", payload: ipData },
-    ];
     const store = mockStore();
     await store.dispatch(fetchIpData());
     expect(store.getActions());
@@ -62,10 +58,6 @@ describe("actions", () => {
     const error = "Request failed with status code 404";
     const response = { status: "fail", message: error };
     mockAxios.onGet("http://ip-api.com/json/1.1.1.1").replyOnce(404, response);
-    const expectedActions = [
-      { type: "FETCH_IP_DATA_REQUEST" },
-      { type: "FETCH_IP_DATA_FAILURE", payload: error },
-    ];
     const store = mockStore();
     await store.dispatch(fetchIpData());
     expect(store.getActions());
